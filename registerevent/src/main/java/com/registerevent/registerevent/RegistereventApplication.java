9ff1e89b7fbb926657c4f0a47efa862dd37d2018
gil-son <gilszon@gmail.com>
2021-04-13 19:39:47 -0300

    Default Files

---
+++ registerevent/src/main/java/com/registerevent/registerevent/RegistereventApplication.java
@@ -0,0 +1,14 @@
+package com.registerevent.registerevent;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistereventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistereventApplication.class, args);
+	}
+
+}
---
+++ registerevent/src/test/java/com/registerevent/registerevent/RegistereventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.registerevent.registerevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistereventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
