d5c39171f3e0c256a9820bb2c3af52c3e5917a63
gil-son <gilszon@gmail.com>
2021-04-13 20:34:21 -0300

    Model

---
+++ registerevent/src/main/java/com/registerevent/model/Event.java
@@ -0,0 +1,39 @@
+package com.registerevent.model;
+
+public class Event {
+	private String name;
+	private String locale;
+	private String date;
+	private String schedule;
+	
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLocale() {
+		return locale;
+	}
+	public void setLocale(String locale) {
+		this.locale = locale;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getSchedule() {
+		return schedule;
+	}
+	public void setSchedule(String schedule) {
+		this.schedule = schedule;
+	}
+	
+	
+	
+	
+}
