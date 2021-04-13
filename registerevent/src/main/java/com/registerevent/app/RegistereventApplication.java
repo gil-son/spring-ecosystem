f622935001cef025fbed1cbfde5ab4e31b62b3c0
gil-son <gilszon@gmail.com>
2021-04-13 20:31:19 -0300

    Register Event Application

--- registerevent/src/main/java/com/registerevent/app/RegistereventApplication.java
+++ registerevent/src/main/java/com/registerevent/app/RegistereventApplication.java
@@ -1,10 +1,13 @@
-package com.registerevent.registerevent;
+package com.registerevent.app;
 
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
+@ComponentScan({"com.registerevent.controller"})
 public class RegistereventApplication {
 
 	public static void main(String[] args) {
