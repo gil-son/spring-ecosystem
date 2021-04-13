bfaeb13baad7f7ca5be3c9eb5315daea42765b1f
gil-son <gilszon@gmail.com>
2021-04-13 20:31:53 -0300

    Index Controller

--- registerevent/src/main/java/com/registerevent/controller/IndexController.java
+++ registerevent/src/main/java/com/registerevent/controller/IndexController.java
@@ -1,4 +1,4 @@
-package com.registerevent.registerevent;
+package com.registerevent.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
