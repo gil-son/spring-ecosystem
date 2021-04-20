be073a8fe638f03444a29cda9bfd29c914d0afb0
gil-son <gilszon@gmail.com>
2021-04-20 00:15:02 -0300

    Spring-initializr

---
+++ mini-projects/algafood-api/src/test/java/com/algaworks/food/AlgafoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.food;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgafoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
