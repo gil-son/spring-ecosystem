5da03cb08726b60eb1927bf305d33b07e2377ced
gil-son <gilszon@gmail.com>
2021-04-25 08:11:23 -0300

    DevSuperior Domain Model

---
+++ mini-projects/domainmodel/src/main/java/com/devsuperior/domainmodel/DomainmodelApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.domainmodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomainmodelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomainmodelApplication.class, args);
+	}
+
+}
---
+++ mini-projects/domainmodel/src/test/java/com/devsuperior/domainmodel/DomainmodelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.domainmodel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomainmodelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
