085c86ab3ea318aefbf5263c08b726cb2fa1b65d
gil-son <gilszon@gmail.com>
2021-04-26 05:03:39 -0300

    Entity Client

--- mini-projects/domainmodel/src/main/java/com/devsuperior/domainmodel/entities/Client.java
+++ mini-projects/domainmodel/src/main/java/com/devsuperior/domainmodel/entities/Client.java
@@ -7,29 +7,29 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity // Annotation for JPA. This is a Entity
-@Table(name= "tb_client")
+@Table(name="tb_client")
 
 public class Client {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY) // Inform database to creat a primary key auto_increment
-	private int id;
+	private Integer id; // Interger accept null
 	private String name;
 	private String email;
 	
 	public Client() {}
 	
-	public Client(int id, String name, String email) {
+	public Client(Integer id, String name, String email) {
 		this.id = id;
 		this.name = name;
 		this.email = email;
 	}
 
 	
-	public int getId() {
+	public Integer getId() {
 		return id;
 	}
 
-	public void setId(int id) {
+	public void setId(Integer id) {
 		this.id = id;
 	}
 
