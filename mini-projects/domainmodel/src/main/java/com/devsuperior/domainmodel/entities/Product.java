8abcb9e2858979766278e96e1fb92e25be5ca174
gil-son <gilszon@gmail.com>
2021-04-26 05:05:19 -0300

    Entity Product

---
+++ mini-projects/domainmodel/src/main/java/com/devsuperior/domainmodel/entities/Product.java
@@ -0,0 +1,54 @@
+package com.devsuperior.domainmodel.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tb_product")
+public class Product {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String name;
+	private Double price;
+	 
+	public void Product(){}
+
+	public Product(Integer id, String name, Double price) {
+		this.id = id;
+		this.name = name;
+		this.price = price;
+	}
+
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	 
+	 
+	 
+}
