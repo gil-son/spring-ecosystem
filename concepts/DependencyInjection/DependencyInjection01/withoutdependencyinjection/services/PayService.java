5596db86eb0c200d939470c5daca19d6c81b9400
gil-son <gilszon@gmail.com>
2021-04-27 00:46:44 -0300

    Pay Service - Without Dependency Injection

--- concepts/DependencyInjection/DependencyInjection01/withoutdependencyinjection/services/PayService.java
+++ concepts/DependencyInjection/DependencyInjection01/withoutdependencyinjection/services/PayService.java
@@ -1,5 +1,10 @@
 package services;
 
 public class PayService {
-
+	DeductionService deductionService = new MGDeductionService();
+	
+	public double tax(double amount) {
+		amount = amount - deductionService.deduction(amount);
+		return amount * 0.2;
+	}
 }
