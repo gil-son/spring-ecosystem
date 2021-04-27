9f542dc38944bb52279ce725b57302c8b31cc378
gil-son <gilszon@gmail.com>
2021-04-27 00:48:23 -0300

    SP Deduction Service - Without Dependency Injection

--- concepts/DependencyInjection/DependencyInjection01/withoutdependencyinjection/services/SPDeductionService.java
+++ concepts/DependencyInjection/DependencyInjection01/withoutdependencyinjection/services/SPDeductionService.java
@@ -1,5 +1,9 @@
 package services;
 
-public class SPDeductionService {
+public class SPDeductionService implements DeductionService{
 
+	@Override
+	public double deduction(double amount) {
+		return amount * 0.12;
+	}
 }
