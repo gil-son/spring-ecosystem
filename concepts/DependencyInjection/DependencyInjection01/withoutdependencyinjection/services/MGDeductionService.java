611057cb56dbc61286a94c33f16da735911a3173
gil-son <gilszon@gmail.com>
2021-04-27 00:48:53 -0300

    MG Deduction Service - Without Dependency Injection

--- concepts/DependencyInjection/DependencyInjection01/withoutdependencyinjection/services/MGDeductionService.java
+++ concepts/DependencyInjection/DependencyInjection01/withoutdependencyinjection/services/MGDeductionService.java
@@ -1,5 +1,9 @@
 package services;
 
-public class MGDeductionService {
+public class MGDeductionService implements DeductionService{
 
+	@Override
+	public double deduction(double amount) {
+		return amount * 0.12;
+	}
 }
