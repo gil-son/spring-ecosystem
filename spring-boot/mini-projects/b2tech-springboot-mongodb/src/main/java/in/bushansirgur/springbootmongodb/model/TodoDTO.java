53cd8af5a5e9f2449f968ed4c7eeaf02ac4c66fd
gil-son <gilszon@gmail.com>
2021-04-30 15:11:16 -0300

    Configure without Lombok

--- spring-boot/mini-projects/b2tech-springboot-mongodb/src/main/java/in/bushansirgur/springbootmongodb/model/TodoDTO.java
+++ spring-boot/mini-projects/b2tech-springboot-mongodb/src/main/java/in/bushansirgur/springbootmongodb/model/TodoDTO.java
@@ -7,12 +7,15 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+
+@Getter 
 @Setter
-@Getter
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Document(collection="todos")
@@ -23,6 +26,49 @@ public class TodoDTO {
 	 private String todo;
 	 private String description;
 	 private Boolean completed; 
-	 private Date createAt;
+	 
+	 
+	 @Getter @Setter private Date createAt;
+	 
+	 
 	 private Date updateAt;
+	 
+	 
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getTodo() {
+		return todo;
+	}
+	public void setTodo(String todo) {
+		this.todo = todo;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Boolean getCompleted() {
+		return completed;
+	}
+	public void setCompleted(Boolean completed) {
+		this.completed = completed;
+	}
+	public Date getCreateAt() {
+		return createAt;
+	}
+	public void setCreateAt(Date createAt) {
+		this.createAt = createAt;
+	}
+	public Date getUpdateAt() {
+		return updateAt;
+	}
+	public void setUpdateAt(Date updateAt) {
+		this.updateAt = updateAt;
+	}
+	
 }
