d9bad668bf7fb4f94ff296a96c4cd7af28e39b60
gil-son <gilszon@gmail.com>
2021-04-30 16:16:08 -0300

    Controller - DELETE by Id

--- spring-boot/mini-projects/b2tech-springboot-mongodb/src/main/java/in/bushansirgur/springbootmongodb/controller/TodoController.java
+++ spring-boot/mini-projects/b2tech-springboot-mongodb/src/main/java/in/bushansirgur/springbootmongodb/controller/TodoController.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -74,23 +75,14 @@ public class TodoController {
 	}
 	
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	@DeleteMapping("/todos/{id}") //ResponseEntity<String>
+	public ResponseEntity<?> deleteById(@PathVariable("id") String id){
+		try {
+			todoRepo.deleteById(id);
+			return new ResponseEntity<>("Successfully deleted with id: "+id, HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+		}
+	}
 	
 }
