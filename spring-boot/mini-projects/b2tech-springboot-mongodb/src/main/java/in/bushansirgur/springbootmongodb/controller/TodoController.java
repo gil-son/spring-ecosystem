e53763d3e62df8f27ea8f6ce4d9ff35d03584c80
gil-son <gilszon@gmail.com>
2021-04-30 15:10:10 -0300

    Controller - POST

--- spring-boot/mini-projects/b2tech-springboot-mongodb/src/main/java/in/bushansirgur/springbootmongodb/controller/TodoController.java
+++ spring-boot/mini-projects/b2tech-springboot-mongodb/src/main/java/in/bushansirgur/springbootmongodb/controller/TodoController.java
@@ -1,11 +1,14 @@
 package in.bushansirgur.springbootmongodb.controller;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import in.bushansirgur.springbootmongodb.model.TodoDTO;
@@ -26,4 +29,44 @@ public class TodoController {
 			return new ResponseEntity<>("No To do(s) available", HttpStatus.NOT_FOUND);
 		}
 	}
+	
+	
+	@PostMapping("/todos")
+	public ResponseEntity<?> createTodo(@RequestBody TodoDTO todo){
+		try {
+			todo.setCreateAt( new Date(System.currentTimeMillis()));
+			todoRepo.save(todo);
+			return new ResponseEntity<TodoDTO>(todo, HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+	
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
