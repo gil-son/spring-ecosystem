8b9e36fd89b789091c031ef3b81ed4e0dde9528b
gil-son <gilszon@gmail.com>
2021-04-30 13:47:50 -0300

    Controller

---
+++ spring-boot/mini-projects/b2tech-springboot-mongodb/src/main/java/in/bushansirgur/springbootmongodb/controller/TodoController.java
@@ -0,0 +1,29 @@
+package in.bushansirgur.springbootmongodb.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import in.bushansirgur.springbootmongodb.model.TodoDTO;
+import in.bushansirgur.springbootmongodb.repository.TodoRepository;
+
+@RestController
+public class TodoController {
+	
+	@Autowired
+	private TodoRepository todoRepo;
+	
+	@GetMapping("/todos")
+	public ResponseEntity<?> getAllTodos(){
+		List<TodoDTO> todos = todoRepo.findAll();
+		if(todos.size() > 0) {
+			return new ResponseEntity<List<TodoDTO>>(todos, HttpStatus.OK);
+		}else {
+			return new ResponseEntity<>("No To do(s) available", HttpStatus.NOT_FOUND);
+		}
+	}
+}
