7a2b706a02dacd52ba5a85f53e2cda5994d660f0
gil-son <gilszon@gmail.com>
2021-04-30 15:29:15 -0300

    Controller - GET by Id

--- spring-boot/mini-projects/b2tech-springboot-mongodb/src/main/java/in/bushansirgur/springbootmongodb/controller/TodoController.java
+++ spring-boot/mini-projects/b2tech-springboot-mongodb/src/main/java/in/bushansirgur/springbootmongodb/controller/TodoController.java
@@ -2,11 +2,13 @@ package in.bushansirgur.springbootmongodb.controller;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,11 +42,18 @@ public class TodoController {
 		} catch (Exception e) {
 			return new ResponseEntity<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
 		}
-		
-	
 	}
 	
 	
+	@GetMapping("/todos/{id}")
+	public ResponseEntity<?> getSingleTodo(@PathVariable("id") String id){
+		Optional<TodoDTO> todoOptional = todoRepo.findById(id);
+		if(todoOptional.isPresent()) {
+			return new ResponseEntity<>(todoOptional.get(), HttpStatus.OK);
+		}else {
+			return new ResponseEntity<>("To do not found with id: "+id,HttpStatus.NOT_FOUND);
+		}
+	}
 	
 	
 	
