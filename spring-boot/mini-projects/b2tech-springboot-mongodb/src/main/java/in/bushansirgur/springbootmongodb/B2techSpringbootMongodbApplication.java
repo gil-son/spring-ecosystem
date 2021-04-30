b660dd086ba7688ea15287531381676ab2e4b994
gil-son <gilszon@gmail.com>
2021-04-30 12:10:44 -0300

    Spring Boot with MongoDB and CRUD

---
+++ spring-boot/mini-projects/b2tech-springboot-mongodb/src/main/java/in/bushansirgur/springbootmongodb/B2techSpringbootMongodbApplication.java
@@ -0,0 +1,13 @@
+package in.bushansirgur.springbootmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B2techSpringbootMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B2techSpringbootMongodbApplication.class, args);
+	}
+
+}
---
+++ spring-boot/mini-projects/b2tech-springboot-mongodb/src/test/java/in/bushansirgur/springbootmongodb/B2techSpringbootMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package in.bushansirgur.springbootmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B2techSpringbootMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
