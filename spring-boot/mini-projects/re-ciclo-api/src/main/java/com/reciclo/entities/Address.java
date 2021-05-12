3bf8f2834ece5555d5ef03e7e0afa78846e733ec
gil-son <gilszon@gmail.com>
2021-05-12 08:16:31 -0300

    Initial

---
+++ spring-boot/mini-projects/re-ciclo-api/src/main/java/com/reciclo/entities/Address.java
@@ -0,0 +1,15 @@
+package com.reciclo.entities;
+
+import java.util.Date;
+
+public class Address {
+	
+	private Integer id;
+	private String zipCode;
+	private Integer publicPlace;
+	private String complement; 
+	private String district;
+	private String locality;
+	private String State;
+	private Integer number;
+}
---
+++ spring-boot/mini-projects/re-ciclo-api/src/main/java/com/reciclo/entities/User.java
@@ -0,0 +1,15 @@
+package com.reciclo.entities;
+
+import java.util.Date;
+
+public class User {
+	
+	private Integer id;
+	private String name;
+	private Integer adm;
+	private String email; 
+	private String phone;
+	private String password;
+	private Date date;
+	
+}
