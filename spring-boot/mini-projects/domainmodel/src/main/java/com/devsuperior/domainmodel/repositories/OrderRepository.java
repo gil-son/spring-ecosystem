063462eb08ee69dd849b76100a2fd1d38f238bed
gil-son <gilszon@gmail.com>
2021-05-13 18:24:12 -0300

    Order Repository

--- spring-boot/mini-projects/domainmodel/src/main/java/com/devsuperior/domainmodel/repositories/OrderRepository.java
+++ spring-boot/mini-projects/domainmodel/src/main/java/com/devsuperior/domainmodel/repositories/OrderRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.devsuperior.domainmodel.entities.Order;
 
 public interface OrderRepository extends JpaRepository<Order, Integer>{
-
+	
 }
