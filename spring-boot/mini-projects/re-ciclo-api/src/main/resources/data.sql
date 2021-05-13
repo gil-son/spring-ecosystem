3e17a8327e273da08310d372dcf315662f1c3cc9
gil-son <gilszon@gmail.com>
2021-05-13 19:45:59 -0300

    Data logging

---
+++ spring-boot/mini-projects/re-ciclo-api/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_users(name, owner, email, phone, password, date ) VALUES ('FulanoA',1,'fulanoagmail.com','3333-3333','123456', CURRENT_TIMESTAMP);
+INSERT INTO tb_users(name, owner, email, phone, password, date ) VALUES ('FulanoB',1,'fulanogmail.com','3333-3333','123456', CURRENT_TIMESTAMP);
+
