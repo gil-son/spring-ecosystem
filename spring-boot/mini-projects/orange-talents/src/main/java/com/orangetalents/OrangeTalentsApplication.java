c9a89d9b170b148e3b9e7bdb31277eaaac4b4575
gil-son <gilszon@gmail.com>
2021-05-16 14:54:44 -0300

    Initial

---
+++ spring-boot/mini-projects/orange-talents/src/main/java/com/orangetalents/OrangeTalentsApplication.java
@@ -0,0 +1,13 @@
+package com.orangetalents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrangeTalentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrangeTalentsApplication.class, args);
+	}
+
+}
---
+++ spring-boot/mini-projects/orange-talents/src/test/java/com/orangetalents/OrangeTalentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orangetalents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrangeTalentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
