16eeed70810d3006cced063af586c8ad3711bdfe
gil-son <gilszon@gmail.com>
2021-05-16 17:44:46 -0300

    User Controller

--- spring-boot/mini-projects/re-ciclo-api/src/main/java/com/reciclo/controllers/UserController.java
+++ spring-boot/mini-projects/re-ciclo-api/src/main/java/com/reciclo/controllers/UserController.java
@@ -3,8 +3,12 @@ package com.reciclo.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.reciclo.entities.User;
@@ -21,5 +25,15 @@ public class UserController {
 	public List<User> list(){
 		return userRepository.findAll();
 	}
+	
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public User add(@RequestBody User user) {
+		return userRepository.save(user);
+	}
+	
+	
+	
 	
 }
