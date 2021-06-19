43d4fa2011679d9824460836d5f8778912f5b7d7
gil-son <gilszon@gmail.com>
2021-06-19 17:08:39 -0300

    Controller

--- spring-boot/mini-projects/consume-api-with-webclient/prices/src/main/java/com/price/controller/PriceController.java
+++ spring-boot/mini-projects/consume-api-with-webclient/prices/src/main/java/com/price/controller/PriceController.java
@@ -1,15 +1,40 @@
 package com.price.controller;
 
+
+
+import java.math.BigDecimal;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.price.model.Price;
 
 @RestController
 @RequestMapping("/prices")
 public class PriceController {
 	
+	
+	@GetMapping("/{code}")
+	public ResponseEntity<Price> isPrice(@PathVariable Long code){
+		
+		Price p1 = new Price(code, new BigDecimal("100"));
+		
+		return ResponseEntity.ok(p1);
+	}
+	
+	
+	
+	
+	
 	@GetMapping
 	public String test() {
 		return "Price";
 	}
+	
+	
+	
+	
 }
