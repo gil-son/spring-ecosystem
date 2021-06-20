4da63674f9b470ec04fccffcc0675d6e07b70539
gil-son <gilszon@gmail.com>
2021-06-20 10:13:57 -0300

    Create 'Bean' to use Web Client anywhere

--- spring-boot/mini-projects/consume-api-with-webclient/consumer/src/main/java/com/consumer/ConsumerApplication.java
+++ spring-boot/mini-projects/consume-api-with-webclient/consumer/src/main/java/com/consumer/ConsumerApplication.java
@@ -2,10 +2,23 @@ package com.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class ConsumerApplication {
 
+	@Bean
+	public WebClient webClient(WebClient.Builder builder) {
+		return builder
+				.baseUrl("http://localhost:8081")
+				.defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+				.build();
+	}
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ConsumerApplication.class, args);
 	}
