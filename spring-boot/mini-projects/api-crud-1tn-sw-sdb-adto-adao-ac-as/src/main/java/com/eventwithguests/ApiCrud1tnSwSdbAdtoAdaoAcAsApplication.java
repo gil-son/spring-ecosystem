a47680312f087966762066dc9df5eba8eda65194
gil-son <gilszon@gmail.com>
2021-07-13 18:28:02 -0300

    API to manage an event with guests

--- spring-boot/mini-projects/api-crud-1tn-sw-sdb-adto-adao-ac-as/src/main/java/com/eventwithguests/ApiCrud1tnSwSdbAdtoAdaoAcAsApplication.java
+++ spring-boot/mini-projects/api-crud-1tn-sw-sdb-adto-adao-ac-as/src/main/java/com/eventwithguests/ApiCrud1tnSwSdbAdtoAdaoAcAsApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiCrud1tnSdbAdtoAdaoAcAsApplication {
+public class ApiCrud1tnSwSdbAdtoAdaoAcAsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiCrud1tnSdbAdtoAdaoAcAsApplication.class, args);
+		SpringApplication.run(ApiCrud1tnSwSdbAdtoAdaoAcAsApplication.class, args);
 	}
 
 }
--- spring-boot/mini-projects/api-crud-1tn-sw-sdb-adto-adao-ac-as/src/test/java/com/eventwithguests/ApiCrud1tnSwSdbAdtoAdaoAcAsApplicationTests.java
+++ spring-boot/mini-projects/api-crud-1tn-sw-sdb-adto-adao-ac-as/src/test/java/com/eventwithguests/ApiCrud1tnSwSdbAdtoAdaoAcAsApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApiCrud1tnSdbAdtoAdaoAcAsApplicationTests {
+class ApiCrud1tnSwSdbAdtoAdaoAcAsApplicationTests {
 
 	@Test
 	void contextLoads() {
