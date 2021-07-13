405c191d9ea1d608eba055349ac23ebf737b3c97
gil-son <gilszon@gmail.com>
2021-07-12 22:07:00 -0300

    Event

--- spring-boot/mini-projects/api-crud-1tn-sdb-adto-adao-ac-as/src/main/java/com/eventwithguests/entities/Event.java
+++ spring-boot/mini-projects/api-crud-1tn-sdb-adto-adao-ac-as/src/main/java/com/eventwithguests/entities/Event.java
@@ -1,16 +1,25 @@
 package com.eventwithguests.entities;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
-public class Event {
+@Entity
+@Table(name = "tb_event")
+public class Event implements Serializable{
+	private static final long serialVersionUID = 1L;
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	private String name;
+	private String description;
 	private String local;
 	
 	@OneToMany
@@ -18,19 +27,17 @@ public class Event {
 	
 	public Event() {}
 	
-	public Event(String name, String local) {
-		this.name = name;
+	public Event(String description, String local) {
+		this.description = description;
 		this.local = local;
 	}
 
-	public String getName() {return name;}
+	public String getdescription() {return description;}
 
-	public void setName(String name) {this.name = name;}
+	public void setdescription(String description) {this.description = description;}
 
 	public String getLocal() {return local;}
 
 	public void setLocal(String local) {this.local = local;}
-	
-	
 	
 }
