59e41bcbb444a5cb06f5ed97f14136e3cf4608d6
gil-son <gilszon@gmail.com>
2021-07-12 22:11:33 -0300

    Repository

---
+++ spring-boot/mini-projects/api-crud-1tn-sdb-adto-adao-ac-as/src/main/java/com/eventwithguests/repository/GuestRepository.java
@@ -0,0 +1,11 @@
+package com.eventwithguests.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.eventwithguests.entities.Guest;
+
+@Repository
+public interface GuestRepository extends JpaRepository<Long, Guest> {
+
+}
