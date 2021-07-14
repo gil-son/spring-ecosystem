949e6e7f19e1ac47613f1b8b5835696685748ead
gil-son <gilszon@gmail.com>
2021-07-13 21:11:37 -0300

    Services

--- spring-boot/mini-projects/api-crud-1tn-sw-sdb-adto-adao-ac-as/src/main/java/com/eventwithguests/services/EventService.java
+++ spring-boot/mini-projects/api-crud-1tn-sw-sdb-adto-adao-ac-as/src/main/java/com/eventwithguests/services/EventService.java
@@ -3,11 +3,23 @@ package com.eventwithguests.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.eventwithguests.dto.EventDTO;
+import com.eventwithguests.entities.Event;
+
 @Service
 public class EventService {
 
 	@Autowired
 	EventService eventService;
 	
-	
+	public EventDTO insert(EventDTO dto) {
+		Event entity = new Event();
+		
+		entity.setdescription(dto.getDescription());
+		entity.setLocal(dto.getLocal());
+		
+		entity.getGuests().clear();
+		
+		return null;
+	}
 }
