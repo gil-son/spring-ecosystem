84d2e037ab1ebbe8672a5f0c66be3eb58a293529
gil-son <gilszon@gmail.com>
2021-07-13 21:12:51 -0300

    Resources

--- spring-boot/mini-projects/api-crud-1tn-sw-sdb-adto-adao-ac-as/src/main/java/com/eventwithguests/resources/EventResource.java
+++ spring-boot/mini-projects/api-crud-1tn-sw-sdb-adto-adao-ac-as/src/main/java/com/eventwithguests/resources/EventResource.java
@@ -1,15 +1,38 @@
 package com.eventwithguests.resources;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.eventwithguests.dto.EventDTO;
+import com.eventwithguests.services.EventService;
 
 @RestController
 @RequestMapping(value = "/events")
 public class EventResource {
 
+	private EventService eventService;
+	
+	public EventResource(EventService eventService) {
+		this.eventService = eventService;
+	}
+	
+	
+	@PostMapping
+	public ResponseEntity<EventDTO> insert(@RequestBody EventDTO dto){
+		
+		return null;
+	}
+	
+	
 	@GetMapping
 	public String test() {
 		return "Test";
 	}
+	
+	
+	
 }
