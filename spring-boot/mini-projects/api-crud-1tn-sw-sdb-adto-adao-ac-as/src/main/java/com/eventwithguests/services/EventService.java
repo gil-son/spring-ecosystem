a698b41081f16ce6e0f0ae34feb202d73194754f
gil-son <gilszon@gmail.com>
2021-07-13 21:46:18 -0300

    Insert

--- spring-boot/mini-projects/api-crud-1tn-sw-sdb-adto-adao-ac-as/src/main/java/com/eventwithguests/services/EventService.java
+++ spring-boot/mini-projects/api-crud-1tn-sw-sdb-adto-adao-ac-as/src/main/java/com/eventwithguests/services/EventService.java
@@ -4,13 +4,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.eventwithguests.dto.EventDTO;
+import com.eventwithguests.dto.GuestDTO;
 import com.eventwithguests.entities.Event;
+import com.eventwithguests.entities.Guest;
+import com.eventwithguests.repository.EventRepository;
+import com.eventwithguests.repository.GuestRepository;
 
 @Service
 public class EventService {
 
 	@Autowired
-	EventService eventService;
+	EventRepository eventRepository;
+	
+	@Autowired
+	GuestRepository guestRepository;
 	
 	public EventDTO insert(EventDTO dto) {
 		Event entity = new Event();
@@ -20,6 +27,13 @@ public class EventService {
 		
 		entity.getGuests().clear();
 		
-		return null;
+		for(GuestDTO guestDTO : dto.getGuest()) {
+			Guest guest = guestRepository.getById(guestDTO.getId());
+			entity.getGuests().add(guest);
+		}
+		
+		entity = eventRepository.save(entity);
+		
+		return new EventDTO(entity);
 	}
 }
