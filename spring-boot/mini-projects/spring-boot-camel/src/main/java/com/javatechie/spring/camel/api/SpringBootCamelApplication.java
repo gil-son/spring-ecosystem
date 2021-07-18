24e7902d88219a83cf76a1906205a24ced0dec5a
gil-son <gilszon@gmail.com>
2021-07-18 20:58:00 -0300

    Methods to get csv files

--- spring-boot/mini-projects/spring-boot-camel/src/main/java/com/javatechie/spring/camel/api/SpringBootCamelApplication.java
+++ spring-boot/mini-projects/spring-boot-camel/src/main/java/com/javatechie/spring/camel/api/SpringBootCamelApplication.java
@@ -1,5 +1,7 @@
 package com.javatechie.spring.camel.api;
 
+import java.util.Arrays;
+
 import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.boot.SpringApplication;
@@ -19,7 +21,9 @@ public class SpringBootCamelApplication extends RouteBuilder{
 		System.out.println("Started...");
 		// moveAllFiles();
 		// moveSpecificFile("myFile");
-		moveSpecificFileWithBody("Content 1");
+		// moveSpecificFileWithBody("Specific content");
+		// fileProcess();
+		multiFileProcessor();
 		System.out.println("End...");
 	}
 	
@@ -29,12 +33,39 @@ public class SpringBootCamelApplication extends RouteBuilder{
 	}
 
 	public void moveSpecificFile(String type) throws Exception{
-		from("file:C:/Users/gilso/Desktop/a").filter(header(Exchange.FILE_NAME).startsWith(type))
+		from("file:C:/Users/gilso/Desktop/a")
+		.filter(header(Exchange.FILE_NAME).startsWith(type))
 		.to("file:C:/Users/gilso/Desktop/b");
 	}
 	
 	public void moveSpecificFileWithBody(String content) throws Exception{
-		from("file:C:/Users/gilso/Desktop/a").filter(body().contains(content))
-		.to("file:C:/Users/gilso/Desktop/b");
+		from("file:C:/Users/gilso/Desktop/Repositories/gil-son/spring-ecosystem/spring-boot/mini-projects/spring-boot-camel/source")
+		// .filter(body().contains(content)) 
+		.filter(body().startsWith(content))
+		.to("file:C:/Users/gilso/Desktop/Repositories/gil-son/spring-ecosystem/spring-boot/mini-projects/spring-boot-camel/destination");
 	}
+	
+	
+	public void fileProcess(){
+		from("file:a?noop=true").process( p -> {
+			String body = p.getIn().getBody(String.class);
+			StringBuilder sb = new StringBuilder();
+			Arrays.stream(body.split(" ")).forEach( s -> {
+				sb.append( s + ",");
+			});
+			p.getIn().setBody(sb);
+		})
+		
+		// .to("file:b");
+		.to("file:b?fileName=records.csv");
+	}
+	
+	
+	public void multiFileProcessor() {
+		from("file:a?noop=true").unmarshal().csv().split(body().tokenize(",")).choice()
+		.when(body().contains("LastNameA")).to("file:b?fileName=lastnamea.csv")
+		.when(body().contains("LastNameB")).to("file:b?fileName=lastnameb.csv")
+		.when(body().contains("LastNameC")).to("file:b?fileName=lastnamec.csv");
+	}
+	
 }
