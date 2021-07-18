d3bb0bafd06bf4f2e776b7d2812ffb094c7fedbf
gil-son <gilszon@gmail.com>
2021-07-18 18:38:03 -0300

    Apache Camel

--- spring-boot/mini-projects/spring-boot-camel/src/main/java/com/javatechie/spring/camel/api/SpringBootCamelApplication.java
+++ spring-boot/mini-projects/spring-boot-camel/src/main/java/com/javatechie/spring/camel/api/SpringBootCamelApplication.java
@@ -1,13 +1,24 @@
 package com.javatechie.spring.camel.api;
 
+import org.apache.camel.builder.RouteBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootCamelApplication {
+public class SpringBootCamelApplication extends RouteBuilder{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootCamelApplication.class, args);
+	}
+
+	@Override
+	public void configure() throws Exception {
+		// move data from one another file
+		
+		System.out.println("Started...");
+		from("file:C:/Users/gilso/Desktop/a")
+		.to("file:C:/Users/gilso/Desktop/b");
+		System.out.println("End...");
 	}
 
 }
