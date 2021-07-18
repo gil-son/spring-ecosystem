0cb0d795e4b795978118920dc45c52d598eb3d53
gil-son <gilszon@gmail.com>
2021-07-18 17:15:47 -0300

    Ingore

---
+++ spring-boot/mini-projects/spring-boot-camel/src/test/java/com/javatechie/spring/camel/api/SpringBootCamelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatechie.spring.camel.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCamelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
