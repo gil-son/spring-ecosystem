c367e7a863b37ab5ec40917483e3b63ae2734667
gil-son <gilszon@gmail.com>
2021-08-06 20:24:33 -0300

    Spring Boot Camel

--- spring-boot/mini-projects/spring-boot-camel-begin/src/main/java/com/camel/freetech/MyRouteBuilder.java
+++ spring-boot/mini-projects/spring-boot-camel-begin/src/main/java/com/camel/freetech/MyRouteBuilder.java
@@ -1,12 +1,30 @@
 package com.camel.freetech;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
 
+@Component
 public class MyRouteBuilder extends RouteBuilder{
+	
 	@Override
 	public void configure() throws Exception {
-		from("file:src/input?noop=true") //  noop=true = copy file and don't cut
-			.log("${body}");
+		restConfiguration()
+        	.component("servlet");
+
+		rest()
+        	.path("/test")
+        	
+        	.get()
+            .route()
+            .transform(simple("I'm your resource " +
+                        "for all the sports!"))
+        .endRest()
+        
+        .post("/")
+        .route()
+        .to("log:mylogger?showAll=true")
+        .transform(simple("Thanks, sport!"))
+        .endRest();
 		
 	}
 }
--- spring-boot/mini-projects/spring-boot-camel-begin/src/main/java/com/camel/freetech/SpringBootCamelBeginApplication.java
+++ spring-boot/mini-projects/spring-boot-camel-begin/src/main/java/com/camel/freetech/SpringBootCamelBeginApplication.java
@@ -1,19 +1,12 @@
 package com.camel.freetech;
-
-import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.main.Main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootCamelBeginApplication {
 
-	public static void main(String... args) throws Exception{
+	public static void main(String[] args) throws Exception{
 		SpringApplication.run(SpringBootCamelBeginApplication.class, args);
-		
-		Main main = new Main();
-		main.addRouteBuilder(new MyRouteBuilder());
-		main.run(args);
 	}
 
 	
