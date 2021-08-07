290b5cb9ad633c14ce8abb0490098e63c3d5b9ec
gil-son <gilszon@gmail.com>
2021-08-07 11:36:06 -0300

    Person

---
+++ spring-boot/mini-projects/spring-boot-camel-bom-begin/src/main/java/springbootcamelbombegin/Person.java
@@ -0,0 +1,28 @@
+package springbootcamelbombegin;
+
+public class Person {
+	
+	private String name;
+	private int age;
+	private int phone;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getPhone() {
+		return phone;
+	}
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+	
+}
