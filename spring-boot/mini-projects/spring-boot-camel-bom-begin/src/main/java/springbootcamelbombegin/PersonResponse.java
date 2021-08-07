31a1f13b75e63c1f7008e46ef4e16738e7dc10c7
gil-son <gilszon@gmail.com>
2021-08-07 12:00:30 -0300

    Use to create a message

---
+++ spring-boot/mini-projects/spring-boot-camel-bom-begin/src/main/java/springbootcamelbombegin/PersonResponse.java
@@ -0,0 +1,15 @@
+package springbootcamelbombegin;
+
+public class PersonResponse {
+	
+	private String message;
+	
+	public String getMessage() {
+		return this.message;
+	}
+	
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+}
