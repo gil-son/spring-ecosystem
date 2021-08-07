f9ef771dd63172821a56ce49f3cf5a3397e55603
gil-son <gilszon@gmail.com>
2021-08-07 17:41:26 -0300

    Spring Boot Camel with JDBC

---
+++ spring-boot/mini-projects/spring-boot-camel-jdbc/src/main/java/com/springbootcamel/SpringBootCamelJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.springbootcamel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCamelJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCamelJdbcApplication.class, args);
+	}
+
+}
