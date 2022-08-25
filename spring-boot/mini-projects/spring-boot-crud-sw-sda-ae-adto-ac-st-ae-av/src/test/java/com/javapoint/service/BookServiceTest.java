f526f09f287ce598f182e372873e1a1a23c81d96
Gilson Vieira de Souza <gilson@ibm.com>
2022-08-24 23:36:10 -0300

    Testing Service - shouldCreateABook

---
+++ spring-boot/mini-projects/spring-boot-crud-sw-sda-ae-adto-ac-st-ae-av/src/test/java/com/javapoint/service/BookServiceTest.java
@@ -0,0 +1,35 @@
+package com.javapoint.service;
+
+import com.javapoint.entity.Books;
+import com.javapoint.repository.BooksRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mockito;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+
+@SpringBootTest
+public class BookServiceTest {
+
+
+    @InjectMocks // Inject the created instances from mocks
+    private BooksService booksService;
+
+    @MockBean // Execute mock from
+    private BooksRepository booksRepository;
+
+    //@Mock // Instance object with mocks
+    Books books = Mockito.mock(Books.class);
+
+    
+    @Test
+    public void shouldCreateABook(){
+
+        Mockito.when(booksRepository.save(books)).thenReturn(books);
+        Assertions.assertEquals(booksRepository.save(books), books);
+    }
+
+}
