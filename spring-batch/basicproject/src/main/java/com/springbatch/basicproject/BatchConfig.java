83022b3b1d8286b02ca208bbbf502f6363bc82f5
Gilson Vieira de Souza <gilszon@gmail.com>
2022-09-26 20:16:00 -0300

    Basic project

---
+++ spring-batch/basicproject/src/main/java/com/springbatch/basicproject/BatchConfig.java
@@ -0,0 +1,47 @@
+package com.springbatch.basicproject;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@EnableBatchProcessing
+@Configuration
+public class BatchConfig {
+
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    private StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job printHelloJob(){  // job is like a task
+        return jobBuilderFactory
+                .get("printHelloJob")
+                .start(printHelloJobStep()) // any job has step
+                .build();
+    }
+
+    public Step printHelloJobStep(){
+        return stepBuilderFactory
+                .get("printHelloStep")
+                .tasklet(new Tasklet() { // simples tasks
+                    @Override
+                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println("Hello World");
+                        return RepeatStatus.FINISHED;
+                    }
+                })
+                .build();
+    }
+
+}
---
+++ spring-batch/basicproject/src/test/java/com/springbatch/basicproject/BasicprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbatch.basicproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
