71dece03d1596ca18ef39ec39fe5441668206c7b
Gilson Vieira de Souza <gilszon@gmail.com>
2022-10-14 15:33:30 -0300

    Multiple runs and scope

--- spring-batch/proj/basicproject-jobs-postgres/src/main/java/com/springbatch/basicproject/BatchConfig.java
+++ spring-batch/proj/basicproject-jobs-postgres/src/main/java/com/springbatch/basicproject/BatchConfig.java
@@ -6,11 +6,13 @@ import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -36,14 +38,20 @@ public class BatchConfig {
     public Step printHelloJobStep(){
         return stepBuilderFactory
                 .get("printHelloStep")
-                .tasklet(new Tasklet() { // simples tasks
-                    @Override
-                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println("Hello World");
-                        return RepeatStatus.FINISHED;
-                    }
-                })
+                .tasklet(printHelloTasklet(null))
                 .build();
     }
+    @Bean
+    @StepScope
+    public Tasklet printHelloTasklet(@Value("#{jobParameters['name']}") String name){
 
+        return new Tasklet() {
+            @Override
+            public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                System.out.println(String.format("Hello, %s!", name));
+                return RepeatStatus.FINISHED;
+            }
+        };
+
+    }
 }
