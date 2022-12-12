5c1da2b9aa077acb72102ee073a0db231922fbc0
Gilson Vieira de Souza <gilszon@gmail.com>
2022-12-12 15:19:44 -0300

    Creating a hard coded limits service - V2

---
+++ spring-cloud/version2.5-until-the-next-version/project1/limits-service/src/main/java/com/rest/microservices/limitsservice/controller/LimitsController.java
@@ -0,0 +1,15 @@
+package com.rest.microservices.limitsservice.controller;
+
+import com.rest.microservices.limitsservice.bean.Limits;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LimitsController {
+
+    @GetMapping("limits")
+    public Limits retrieveLimits(){
+        return new Limits(1, 1000);
+    }
+
+}
---
+++ spring-cloud/version2.5-until-the-next-version/project1/limits-service/src/test/java/com/rest/microservices/limitsservice/LimitsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rest.microservices.limitsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
