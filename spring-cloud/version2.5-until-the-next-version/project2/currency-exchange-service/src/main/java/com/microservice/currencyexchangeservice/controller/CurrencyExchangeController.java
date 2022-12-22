6b8553c3cf6ae4e3fc81140e83ddba741c224cf0
Gilson Vieira de Souza <gilszon@gmail.com>
2022-12-22 08:10:29 -0300

    Setting up Dynamic Port in the Response

--- spring-cloud/version2.5-until-the-next-version/project2/currency-exchange-service/src/main/java/com/microservice/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ spring-cloud/version2.5-until-the-next-version/project2/currency-exchange-service/src/main/java/com/microservice/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -1,6 +1,8 @@
 package com.microservice.currencyexchangeservice.controller;
 
 import com.microservice.currencyexchangeservice.model.CurrencyExchange;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,11 +12,18 @@ import java.math.BigDecimal;
 @RestController
 public class CurrencyExchangeController {
 
-            // "http://localhost:8000/currency-exchange/from/USD/to/INR"
+    @Autowired
+    private Environment environment;
+
+            // "http://localhost:8000/currency-exchange/from/USD/to/INR" //8001
     @GetMapping("/currency-exchange/{from}/USD/{to}/INR")
     public CurrencyExchange retrieveExchangeValue(@PathVariable String from,
                                                   @PathVariable String to){
-        return new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(50));
+        CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(50));
+        String port = environment.getProperty("local.server.port");
+        currencyExchange.setEnvironment(port); // "8080"
+        return currencyExchange;
+    
     }
 
 }
--- spring-cloud/version2.5-until-the-next-version/project2/currency-exchange-service/src/main/java/com/microservice/currencyexchangeservice/model/CurrencyExchange.java
+++ spring-cloud/version2.5-until-the-next-version/project2/currency-exchange-service/src/main/java/com/microservice/currencyexchangeservice/model/CurrencyExchange.java
@@ -8,6 +8,7 @@ public class CurrencyExchange {
     private String from;
     private String to;
     private BigDecimal conversionMultiple;
+    private String environment;
 
     public CurrencyExchange(){}
 
@@ -49,5 +50,13 @@ public class CurrencyExchange {
 
     public void setConversionMultiple(BigDecimal conversionMultiple) {
         this.conversionMultiple = conversionMultiple;
+    }
+
+    public String getEnvironment() {
+        return environment;
+    }
+
+    public void setEnvironment(String environment) {
+        this.environment = environment;
     }
 }
