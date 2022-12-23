82053ae1add0b8baa295c92245e89121f7548cb8
Gilson Vieira de Souza <gilszon@gmail.com>
2022-12-23 07:13:47 -0300

    Configure JPA and Initialized Data

--- spring-cloud/version2.5-until-the-next-version/project2/currency-exchange-service/src/main/java/com/microservice/currencyexchangeservice/model/CurrencyExchange.java
+++ spring-cloud/version2.5-until-the-next-version/project2/currency-exchange-service/src/main/java/com/microservice/currencyexchangeservice/model/CurrencyExchange.java
@@ -1,11 +1,19 @@
 package com.microservice.currencyexchangeservice.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
 import java.math.BigDecimal;
 
+@Entity
 public class CurrencyExchange {
 
+    @Id
     private Long id;
+    @Column(name = "currency_from")
     private String from;
+    @Column(name = "currency_to")
     private String to;
     private BigDecimal conversionMultiple;
     private String environment;
