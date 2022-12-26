5e60ded19dee75453ca0e78cb7181db2d59ff8e7
Gilson Vieira de Souza <gilszon@gmail.com>
2022-12-26 10:20:47 -0300

    Currency Conversion Microservice with Database

---
+++ spring-cloud/version2.5-until-the-next-version/project3/currency-conversion-service/src/main/java/com/conversion/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.conversion.microservices.currencyconversionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyConversionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConversionServiceApplication.class, args);
+	}
+
+}
