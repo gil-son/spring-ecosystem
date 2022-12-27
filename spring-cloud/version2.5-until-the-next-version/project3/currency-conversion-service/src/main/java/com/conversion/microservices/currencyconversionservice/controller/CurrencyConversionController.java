c47b9c2f2efec22ec3fe75cf148f13b84b14f698
Gilson Vieira de Souza <gilszon@gmail.com>
2022-12-27 11:42:13 -0300

    Using Feign REST Client for Service Invocation

--- spring-cloud/version2.5-until-the-next-version/project3/currency-conversion-service/src/main/java/com/conversion/microservices/currencyconversionservice/controller/CurrencyConversionController.java
+++ spring-cloud/version2.5-until-the-next-version/project3/currency-conversion-service/src/main/java/com/conversion/microservices/currencyconversionservice/controller/CurrencyConversionController.java
@@ -1,6 +1,8 @@
 package com.conversion.microservices.currencyconversionservice.controller;
 
 import com.conversion.microservices.currencyconversionservice.entity.CurrencyConversion;
+import com.conversion.microservices.currencyconversionservice.proxy.CurrencyExchangeProxy;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,7 +15,10 @@ import java.util.HashMap;
 @RestController
 public class CurrencyConversionController {
 
-    @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
+    @Autowired
+    private CurrencyExchangeProxy proxy;
+
+    @GetMapping("/currency-conversion-rest-template/from/{from}/to/{to}/quantity/{quantity}")
     // http://localhost:8100/currency-conversion/from/USD/to/INR/quantity/10
     public CurrencyConversion calculateCurrencyConversion(
             @PathVariable String from,
@@ -31,13 +36,38 @@ public class CurrencyConversionController {
 
         CurrencyConversion bodyCurrencyConversion = responseEntity.getBody();
 
-        return new CurrencyConversion(bodyCurrencyConversion.getId(),
-                bodyCurrencyConversion.getFrom(),
-                bodyCurrencyConversion.getTo(),
-                bodyCurrencyConversion.getQuantity(),
+        return new CurrencyConversion(
+                bodyCurrencyConversion.getId(),
+                from,
+                to,
+                quantity,
                 bodyCurrencyConversion.getConversionMultiple(),
                 quantity.multiply(bodyCurrencyConversion.getConversionMultiple()),
-                bodyCurrencyConversion.getEnvironment());
-    };
+                bodyCurrencyConversion.getEnvironment()+" "+"rest template");
+    }
+
+
+    @GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+    // http://localhost:8100/currency-conversion/from/USD/to/INR/quantity/10
+    public CurrencyConversion calculateCurrencyConversionFeign(
+            @PathVariable String from,
+            @PathVariable String to,
+            @PathVariable BigDecimal quantity
+    ){
+
+        // You can find that microservice in https://github.com/gil-son/spring-ecosystem/tree/main/spring-cloud/version2.5-until-the-next-version/project2/currency-exchange-service
+
+        CurrencyConversion bodyCurrencyConversion = proxy.retrieveExchangeValue(from, to);
+
+        return new CurrencyConversion(
+                bodyCurrencyConversion.getId(),
+                from,
+                to,
+                quantity,
+                bodyCurrencyConversion.getConversionMultiple(),
+                quantity.multiply(bodyCurrencyConversion.getConversionMultiple()),
+                bodyCurrencyConversion.getEnvironment()+" "+"feign");
+    }
+
 
 }
---
+++ spring-cloud/version2.5-until-the-next-version/project3/currency-conversion-service/src/main/java/com/conversion/microservices/currencyconversionservice/proxy/CurrencyExchangeProxy.java
@@ -0,0 +1,15 @@
+package com.conversion.microservices.currencyconversionservice.proxy;
+
+import com.conversion.microservices.currencyconversionservice.entity.CurrencyConversion;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name="currency-exchange", url = "localhost:8000") // Name of the project to consume API. In this case is the project2 , the url
+public interface CurrencyExchangeProxy {
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyConversion retrieveExchangeValue(@PathVariable String from,
+                                                    @PathVariable String to);
+
+}
