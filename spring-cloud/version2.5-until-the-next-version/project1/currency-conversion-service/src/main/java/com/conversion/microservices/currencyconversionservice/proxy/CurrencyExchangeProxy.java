d14fd31d387088cae3e6fbb42cd7ba96b612b307
Gilson Vieira de Souza <gilszon@gmail.com>
2022-12-29 13:04:30 -0300

    Load Balancing with Eureka, Feign and Spring Cloud LoadBalancer

--- spring-cloud/version2.5-until-the-next-version/project1/currency-conversion-service/src/main/java/com/conversion/microservices/currencyconversionservice/proxy/CurrencyExchangeProxy.java
+++ spring-cloud/version2.5-until-the-next-version/project1/currency-conversion-service/src/main/java/com/conversion/microservices/currencyconversionservice/proxy/CurrencyExchangeProxy.java
@@ -5,7 +5,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="currency-exchange", url = "localhost:8000") // Name of the project to consume API. In this case is the project2 , the url
+//@FeignClient(name="currency-exchange", url = "localhost:8000") // Name of the project to consume API. In this case is the project2 , the url
+@FeignClient(name="currency-exchange") // With Load Balance
 public interface CurrencyExchangeProxy {
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
