dde6c760380c311a38de48260ff469639fa272a8
Gilson Vieira de Souza <gilszon@gmail.com>
2022-12-30 13:22:49 -0300

    Exploring Routes with Spring Cloud Gateway

---
+++ spring-cloud/version2.5-until-the-next-version/project1/api-gateway/src/main/java/com/microservices/apigateway/config/ApiGatewayConfiguration.java
@@ -0,0 +1,39 @@
+package com.microservices.apigateway.config;
+
+import org.springframework.cloud.gateway.route.Route;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.Buildable;
+import org.springframework.cloud.gateway.route.builder.PredicateSpec;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.function.Function;
+
+@Configuration
+public class ApiGatewayConfiguration {
+
+    @Bean
+    public RouteLocator gatewayRouter(RouteLocatorBuilder builder){
+        //  Function<PredicateSpec, Buildable<Route>> routeFunction = p -> ...
+        return builder.routes()
+                .route(p -> p.path("/get")
+                .filters(f -> f
+                        .addRequestHeader("MyHeader", "MyURI")
+                        .addRequestParameter("Param", "MyValue"))
+
+                    .uri("http://httpbin.org:80"))
+                .route(p -> p.path("/currency-exchange/**")
+                        .uri("lb://currency-exchange")) // to use Load Balancing
+                .route(p -> p.path("/currency-conversion-rest-template/**")
+                        .uri("lb://currency-conversion")) // to use Load Balancing
+                .route(p -> p.path("/currency-conversion-feign/**")
+                        .uri("lb://currency-conversion")) // to use Load Balancing
+                .route(p -> p.path("/currency-conversion-new/**")
+                        .filters(f -> f.rewritePath( // replace path of endpoint
+                                "/currency-conversion-new/(?<segment>.*)",
+                                "/currency-conversion-feign/${segment}"))
+                        .uri("lb://currency-conversion")) // to use Load Balancing
+                .build();
+    }
+}
