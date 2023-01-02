6d2256fa8a8618df30d054f3888cc84a4eaddf1f
Gilson Vieira de Souza <gilszon@gmail.com>
2023-01-02 13:37:57 -0300

    Playing with Resilience4j - Retry and Fallback Methods

--- spring-cloud/version2.5-until-the-next-version/project1/currency-exchange-service/src/main/java/com/microservice/currencyexchangeservice/controller/CircuitBreakerController.java
+++ spring-cloud/version2.5-until-the-next-version/project1/currency-exchange-service/src/main/java/com/microservice/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -1,13 +1,32 @@
 package com.microservice.currencyexchangeservice.controller;
 
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+
 
 @RestController
 public class CircuitBreakerController {
 
+    private Logger logger =
+            LoggerFactory.getLogger(CircuitBreakerController.class);
+
     @GetMapping("/sample-api")
+    @Retry(name = "sample-api", fallbackMethod = "hardcodedResponse") // Will retry attempts in 'n' times (look in application.properties). If not work, then will call hardcodedResponse
     public String sampleAPI(){
-        return "Sample API";
+        logger.info("Sample Api call received");
+        ResponseEntity<String> forEntity =
+                new RestTemplate().getForEntity("http://localhost:8080/some-application", String.class);
+
+        return forEntity.getBody();
+    }
+
+    public String hardcodedResponse(Exception ex){
+        return "fallback-response";
     }
 }
