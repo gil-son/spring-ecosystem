f475921ca88c9c567283d5752afa57be6e3df4d1
Gilson Vieira de Souza <gilszon@gmail.com>
2023-01-03 11:43:22 -0300

    Exploring Rate Limiting and BulkHead Features of Resilience4j

--- spring-cloud/version2.5-until-the-next-version/project1/currency-exchange-service/src/main/java/com/microservice/currencyexchangeservice/controller/CircuitBreakerController.java
+++ spring-cloud/version2.5-until-the-next-version/project1/currency-exchange-service/src/main/java/com/microservice/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -1,6 +1,8 @@
 package com.microservice.currencyexchangeservice.controller;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,12 +22,17 @@ public class CircuitBreakerController {
     @GetMapping("/sample-api")
     //@Retry(name = "sample-api", fallbackMethod = "hardcodedResponse") // Will retry attempts in 'n' times (look in application.properties). If not work, then will call hardcodedResponse
     @CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
+    // in 10s I want only 2 request to the sample api | 10s => 10000 calls to the sample api
+    @RateLimiter(name="default")
+    //@Bulkhead(name="sample-api")
+    //10s => 10000 calls to the sample api
     public String sampleAPI(){
         logger.info("Sample Api call received");
-        ResponseEntity<String> forEntity =
-                new RestTemplate().getForEntity("http://localhost:8080/some-application", String.class);
-
-        return forEntity.getBody();
+//        ResponseEntity<String> forEntity =
+//                new RestTemplate().getForEntity("http://localhost:8080/some-application", String.class);
+//
+//        return forEntity.getBody();
+        return "sample-api";
     }
 
     public String hardcodedResponse(Exception ex){
