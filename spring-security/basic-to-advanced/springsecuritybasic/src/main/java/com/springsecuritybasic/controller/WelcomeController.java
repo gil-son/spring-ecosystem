01ee005323b2f3f7ae70d6dc17182c076f3ada48
Gilson Vieira de Souza <gilszon@gmail.com>
2023-01-18 11:00:50 -0300

    Creating a simple Spring Boot without Security

---
+++ spring-security/basic-to-advanced/springsecuritybasic/src/main/java/com/springsecuritybasic/controller/WelcomeController.java
@@ -0,0 +1,14 @@
+package com.springsecuritybasic.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String sayWelcome(){
+        return "Welcome to Spring Application without Security";
+    }
+}
