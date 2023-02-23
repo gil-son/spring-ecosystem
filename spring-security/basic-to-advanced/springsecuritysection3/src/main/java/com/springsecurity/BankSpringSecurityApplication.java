b559cb07b62d741dda42d6463a7645ca8102780c
gil-son <gilszon@gmail.com>
2023-02-22 22:48:09 -0300

    Annotations and InMemoryDetailsManager

--- spring-security/basic-to-advanced/springsecuritysection3/src/main/java/com/springsecurity/BankSpringSecurityApplication.java
+++ spring-security/basic-to-advanced/springsecuritysection3/src/main/java/com/springsecurity/BankSpringSecurityApplication.java
@@ -3,9 +3,11 @@ package com.springsecurity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
 
 @SpringBootApplication
-@ComponentScan("com.springsecurity.controller")
+//@ComponentScan("com.springsecurity.controller")
+@ComponentScans({ @ComponentScan("com.springsecurity.controller"), @ComponentScan("com.springsecurity.config")})
 public class BankSpringSecurityApplication {
 
 	public static void main(String[] args) {
--- spring-security/basic-to-advanced/springsecuritysection3/src/main/java/com/springsecurity/config/ProjectSecurityConfig.java
+++ spring-security/basic-to-advanced/springsecuritysection3/src/main/java/com/springsecurity/config/ProjectSecurityConfig.java
@@ -1,17 +1,16 @@
 package com.springsecurity.config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-
 import org.springframework.security.core.userdetails.User;
-
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-
+import javax.sql.DataSource;
 
 @Configuration
 public class ProjectSecurityConfig {
@@ -30,7 +29,7 @@ public class ProjectSecurityConfig {
     @Bean
     public InMemoryUserDetailsManager userDetailsService(){
         /* Approach I were we use withDefaultPasswordEncoder() method while creating the user details*/
-        UserDetails admin = User.withDefaultPasswordEncoder()
+        /*UserDetails admin = User.withDefaultPasswordEncoder()
                 .username("admin")
                 .password("12345")
                 .authorities("admin")
@@ -42,7 +41,36 @@ public class ProjectSecurityConfig {
                 .authorities("read")
                 .build();
 
+        return new InMemoryUserDetailsManager(admin, user);*/
+
+        /*Approach 2 where we use NoOpPasswordEncoder Bean
+        while creating the user details*/
+
+        UserDetails admin = User.withUsername("admin")
+                .password("12345")
+                .authorities("admin")
+                .build();
+
+        UserDetails user = User.withUsername("user")
+                .password("12345")
+                .authorities("read")
+                .build();
+
         return new InMemoryUserDetailsManager(admin, user);
+    }
+
+
+
+    /**
+     * NoOpPasswordEncoder is not recommended for production usage.
+     * Use only for non-prod.
+     *
+     * @return
+     */
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
 
 }
