304fba4ecc545f71a85c66866405cc7e421f582c
gil-son <gilszon@gmail.com>
2023-04-04 17:20:53 -0300

    Spring 3.0.5

--- spring-boot/mini-projects/api-crud-1tn-sw-sdb-adto-adao-ac-as/src/main/java/com/eventwithguests/entities/Event.java
+++ spring-boot/mini-projects/api-crud-1tn-sw-sdb-adto-adao-ac-as/src/main/java/com/eventwithguests/entities/Event.java
@@ -1,15 +1,12 @@
 package com.eventwithguests.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+
 
 @Entity
 @Table(name = "tb_event")
--- spring-boot/mini-projects/api-crud-1tn-sw-sdb-adto-adao-ac-as/src/main/java/com/eventwithguests/entities/Guest.java
+++ spring-boot/mini-projects/api-crud-1tn-sw-sdb-adto-adao-ac-as/src/main/java/com/eventwithguests/entities/Guest.java
@@ -1,13 +1,10 @@
 package com.eventwithguests.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+
 
 @Entity
 @Table(name = "tb_guest")
