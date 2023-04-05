09b12f4c34745b82e64f61e30300e3c876b79ed6
gil-son <gilszon@gmail.com>
2023-04-05 20:57:45 -0300

    Spring 3.0.5

--- spring-boot/mini-projects/domainmodel/src/main/java/com/devsuperior/domainmodel/entities/Order.java
+++ spring-boot/mini-projects/domainmodel/src/main/java/com/devsuperior/domainmodel/entities/Order.java
@@ -5,14 +5,14 @@ import java.util.List; // Interface. Interface can't be instantiated
 import java.util.ArrayList; // Is a Inheritance of List
 
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.JoinColumn;
 
 @Entity
 @Table(name = "tb_order")
--- spring-boot/mini-projects/domainmodel/src/main/java/com/devsuperior/domainmodel/entities/OrderItem.java
+++ spring-boot/mini-projects/domainmodel/src/main/java/com/devsuperior/domainmodel/entities/OrderItem.java
@@ -1,12 +1,12 @@
 package com.devsuperior.domainmodel.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
