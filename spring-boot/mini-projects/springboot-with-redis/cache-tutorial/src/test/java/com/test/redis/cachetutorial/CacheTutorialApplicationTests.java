19e8edfed29651833537895bfb329be57219805f
gil-son <gilszon@gmail.com>
2024-01-11 20:59:09 -0300

    Spring Boot wih Radis

---
+++ spring-boot/mini-projects/springboot-with-redis/cache-tutorial/src/test/java/com/test/redis/cachetutorial/CacheTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.redis.cachetutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CacheTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
